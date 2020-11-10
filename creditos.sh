97bb8263fb34262ca14cf7addcb5c5ee53286cab
RobertoCarrichi <robertocarrichiunam@gmail.com>
2020-11-09 21:07:18 -0600

    Create credits command

---
+++ creditos.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#Este archivo es solo un comando que presenta a los colaboradores del proyecto (:
+
+echo " ";
+echo " ########################################################";
+echo " #       UNIVERSIDAD NACIONAL AUTÓNOMA DE MÉXICO        #";
+echo " #                FACULTAD DE INGENIERÍA                #";
+echo " #                                                      #";
+echo " #  Colaboradores del proyecto:                         #";
+echo " #     - Ortega Lujano, Conic                           #";
+echo " #     - Carrichi de la Cruz, Roberto Carlos            #";
+echo " #                                                      #";
+echo " # Proyecto realizado para el Programa de Tecnología en #";
+echo " # Cómputo, PROTECO.                                    #";
+echo " #                                                      #";
+echo " # Noviembre, 2020.                                     #";
+echo " ########################################################";
+echo " ";
+echo " ";
+
