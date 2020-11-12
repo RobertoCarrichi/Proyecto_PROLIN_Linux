6e9da4fec0e872aa9243434bc8d5bdc7ff74b5ed
conic-ol <60513800+conic-ol@users.noreply.github.com>
2020-11-11 21:21:27 -0600

    Update infosis.sh

---
+++ comandos/infosis.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+sudo apt install neofetch
+clear
+printf "****************************** Aquí podrás ver información del sistema ******************************\n"
+printf "\n*************** Sistema Operativo ***************\n" 
+cat /etc/issue 
+uname -m
+printf "\n*************** Host ***************\n"; 
+uname -n
+
+printf "\n*************** RAM ***************\n"; 
+hwinfo --memory | grep "Memory Size" | cut -c3-
+
+printf "\n*************** Kernel ***************\n"
+uname -r
+
+printf "\n*************** Shell ***************\n";  
+$echo $SHELL --version
+
+printf "\n*************** Resolución ***************\n";
+xdpyinfo | grep 'dimensions:' | cut -c3-
+
+printf "\n*************** Entorno de escritorclio ***************\n"; 
+env|grep XDG_CURRENT_DESKTOP
+
+printf "\n*************** Terminal ***************\n"; 
+neofetch | grep 'Terminal' | cut -c19-
+
+
+printf "\n*************** Nombre de modelo ***************\n"; 
+lscpu | grep 'Nombre del modelo' | cut -c38-
+
+
+printf "\n*************** GPU ***************\n";
+neofetch | grep 'GPU' | cut -c19-
+
+printf "\n*************** Tamaño de memoria ***************\n"; 
+df . -h 
+grep -m 1 "model name" /proc/cpuinfo | cut -c14-
+
+printf "\n*************** Usuario actual ***************\n"; 
+echo $USER
+
+printf "\n*************** Usuarios logeados ***************\n"; 
+w
+
+ 
+
